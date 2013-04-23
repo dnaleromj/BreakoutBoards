6f67ea61894b82b8c0203a05db0f17100cd96c62
joey moreland <joey.moreland@gmail.com>
2013-04-22 22:51:01 -0400

    Initial commit

---
+++ atxmega32a4u/pcb/production/Seeed/mkseeed.sh
@@ -0,0 +1,8 @@
+cp ../../panel/p-x* .
+mv p-xmega*.top p-xmegaRF.GTL
+mv p-xmega*.bot p-xmegaRF.GBL
+mv p-xmega*.smt p-xmegaRF.GTS
+mv p-xmega*.smb p-xmegaRF.GBS
+mv p-xmega*.tslk p-xmegaRF.GTO
+mv p-xmega*.bslk p-xmegaRF.GBO
+zip xmega32A4U-v0.1a-Seeed.zip  p-x*.???
